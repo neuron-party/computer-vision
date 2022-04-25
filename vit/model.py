defe6743812756635a5f119fe530267d06a4408e
kekpy <kekpyds@gmail.com>
2022-04-24 17:08:21 -0700

    cleaned

--- vit/model.py
+++ vit/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from collections import OrderedDict
 
 from vit.network import *
@@ -28,10 +29,13 @@ class VisionTransformer(nn.Module):
         self.seq_len = self.embed_height * self.embed_width + 1 # add one here for token
         
         # network structure
-        self.conv_proj = nn.Conv2d(in_channels=self.input_channels, out_channels=self.dim, kernel_size=self.patch_size, stride=self.patch_size)
-        self.class_token = nn.Parameter(torch.zeros(1, 1, self.dim))
-        self.encoder = Encoder(self.seq_len, self.num_layers, self.dim, self.hidden_dim, self.num_heads, self.dropout, self.attention_dropout)
-        self.heads = nn.Linear(self.dim, self.num_classes)
+        self.patch_embed = nn.Conv2d(in_channels=self.input_channels, out_channels=self.dim, kernel_size=self.patch_size, stride=self.patch_size)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, self.seq_len, self.dim))
+        self.blocks = nn.Sequential(*[
+            EncoderBlock(self.dim, self.hidden_dim, self.num_heads, self.dropout, self.attention_dropout) for i in range(self.num_layers)
+        ])
+        self.head = nn.Linear(self.dim, self.num_classes)
         
         if self.fine_tune is not None:
             self.ft = nn.Linear(self.num_classes, self.fine_tune) # fine_tune is the modified num classes
@@ -39,7 +43,7 @@ class VisionTransformer(nn.Module):
     def process_input(self, x):
         # inputs are of shape (b, c, h, w)
         # outputs should be of shape (b, s, d)
-        x = self.conv_proj(x)
+        x = self.patch_embed(x)
         x = x.flatten(2)
         out = x.permute(0, 2, 1)
         return out
@@ -49,13 +53,14 @@ class VisionTransformer(nn.Module):
         x = self.process_input(x)
         
         # concatenate token
-        token = self.class_token.expand(b, -1, -1)
+        token = self.cls_token.expand(b, -1, -1)
         x = torch.cat([token, x], dim=1)
-        x = self.encoder(x)
+        x = x + self.pos_embed
+        x = self.blocks(x)
         
         # extract token
         x = x[:, 0]
-        out = self.heads(x)
+        out = self.head(x)
         
         # readjust to fine tuning dataset
         if self.fine_tune is not None:
